393a4c2b748c1401991a65431b76c469e9137006
Douglas Muth <dmuth@dmuth.org>
2019-10-13 16:32:09 -0400

    Script to run Twint live.

---
+++ bin/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Wrapper to run our Twint container.
+#
+
+# Errors are fatal
+set -e
+
+#
+# Are we running the full version? (Lite version by default)
+#
+NAME="twint"
+FULL=""
+if test "$1" == "full"
+then
+	NAME="twint-full"
+	FULL="full"
+	shift
+fi
+
+#
+# Change to the parent of this script
+#
+pushd $(dirname $0) > /dev/null
+cd ..
+
+./bin/build.sh ${FULL}
+
+echo "# "
+echo "# Running Docker container..."
+echo "# "
+echo "# Args: $@"
+echo "# "
+docker run -v $(pwd):/mnt dmuth1/${NAME} $@
+
